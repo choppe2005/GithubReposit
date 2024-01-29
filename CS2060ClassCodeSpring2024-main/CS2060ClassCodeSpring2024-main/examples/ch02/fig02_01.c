ef8eec5e492a564600dbe7df76dac871ad60b95c
choppe2005 <156993239+choppe2005@users.noreply.github.com>
2024-01-28 17:25:30 -0700

    Update fig02_01.c

--- CS2060ClassCodeSpring2024-main/CS2060ClassCodeSpring2024-main/examples/ch02/fig02_01.c
+++ CS2060ClassCodeSpring2024-main/CS2060ClassCodeSpring2024-main/examples/ch02/fig02_01.c
@@ -1,30 +1,49 @@
 // fig02_01.c
 // A first program in C.
 #include <stdio.h>
+int a = 1;
 int length = 0 ;
 int width = 0;
 int area = 0;
-int scanfReturn = 0;
+double avg = 0;
+int scanfReturn1 = 0;
+int scanfReturn2 = 0;
 // function main begins program execution 
 int main(void) {
    printf("Welcome to C!\n");
+   do {
+	   puts("enter the length");
+	   scanfReturn1 = scanf("%d", &length);
+	   if (scanfReturn1 == 1 && length >= 0) {
+		   a = 0;
+	   }
+	   else {
+		   printf("invalid input pls try again and enter an integer!?\n");
+		   while (getchar() != '\n');
+			}
+	   } while (a == 1);
+	   a = 1;
+	   do {
+		   puts("enter the width");
 
-   puts("enter the length");
+		   scanfReturn2 = scanf("%d", &width);
+		   if (scanfReturn2 == 1 && width >= 0) {
+			   a = 0;
+		   }
+		   else {
+			   printf("invalid input pls try again and enter an integer!?\n");
+			   while (getchar() != '\n');
+		   }
+	   } while (a == 1);
+	   avg = (length + width) / 2.0;
+	   area = length * width;
 
-   scanf("%d", &length);
+	   printf("average of length and width:%0.1f\n", avg);
+	   printf("length=%d\nwidth=%d\narea=%d", length, width, area);
 
-   puts("enter the Width");
-
-   scanf("%d", &width);
-
-	area = length * width;
-
-	//scanfReturn = scanf();
-
-	printf("length=%d\nwidth=%d\narea=%d",length,width,area);
-
-	return 0;
-} // end function main 
+	   return 0;
+}
+ // end function main 
 
 
 
